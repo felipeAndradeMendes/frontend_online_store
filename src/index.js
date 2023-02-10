dc85928f0f41e04969905a442f5289d29e836017
Bernardo Vargas <bebeco.vargas@hotmail.com>
2023-02-10 15:05:46 -0300

    correção de conflito 2 req3

--- src/index.js
+++ src/index.js
@@ -5,4 +5,6 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(<BrowserRouter><App /></BrowserRouter>); 
